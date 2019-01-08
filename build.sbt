60cc3af3acc303acfea28ff13d8402fbd0fbbbbc
kapinuss <stanislav.kapinus@gmail.com>
2019-01-08 13:25:44 +0300

    initial

---
+++ build.sbt
@@ -0,0 +1,25 @@
+name := "site"
+organization := "ru.kapinuss"
+version := "0.1"
+scalaVersion := "2.12.8"
+
+val akkaVersion = "2.5.19"
+val akkaHttpVersion = "10.1.5"
+val logbackVersion = "1.2.3"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0",
+  "com.iheart" %% "ficus" % "1.4.2"
+)
+
+resolvers ++= Seq("Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/",
+  "SonaType" at "https://oss.sonatype.org/content/groups/public",
+  "Typesafe maven releases" at "http://repo.typesafe.com/typesafe/maven-releases/",
+  "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/")
+
+fork in Test := true
+fork in run := true
+connectInput in run := true
+outputStrategy := Some(StdoutOutput)
---
+++ src/main/scala/front/Front.scala
@@ -0,0 +1,43 @@
+package vio
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
+import vio.utils.Settings.settings
+import scala.concurrent.{ExecutionContextExecutor, Future}
+
+object Front extends App {
+
+  println(settings.titles)
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  val serverSource = Http().bind(interface = "0.0.0.0", port = settings.port)
+
+  val requestHandler: HttpRequest => HttpResponse = {
+    case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+      HttpResponse(entity = HttpEntity(
+        ContentTypes.`text/html(UTF-8)`,
+        "<html><body>Hello world!</body></html>"))
+
+    case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
+      HttpResponse(entity = "PONG!")
+
+    case HttpRequest(GET, Uri.Path("/crash"), _, _, _) =>
+      sys.error("BOOM!")
+
+    case r: HttpRequest =>
+      r.discardEntityBytes() // important to drain incoming HTTP Entity stream
+      HttpResponse(404, entity = "Unknown resource!")
+  }
+
+  val bindingFuture: Future[Http.ServerBinding] =
+    serverSource.to(Sink.foreach { connection =>
+      connection handleWithSyncHandler requestHandler
+    }).run()
+}
