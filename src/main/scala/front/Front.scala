889f702cd172454d5b7cd7fa803876b2912603ae
kapinuss <stanislav.kapinus@gmail.com>
2019-01-10 15:58:03 +0300

    further

--- src/main/scala/front/Front.scala
+++ src/main/scala/front/Front.scala
@@ -3,10 +3,9 @@ package front
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.model.headers.HttpCookie
+import akka.http.scaladsl.model.headers.{HttpCookie, HttpCookiePair}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
 import front.utils.Settings.settings
@@ -17,27 +16,36 @@ object Front extends App {
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-  var counter: Int = 0
-
-  def c = {
-    counter += 1
-    setCookie(HttpCookie("counter", value = counter.toString))
-  } & deleteCookie("userName")
-
   val route =
     get {
-      (pathEndOrSingleSlash & c) {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
-          s"<html><body>index: {${System.currentTimeMillis()}}</body></html>"))
-      } ~
-        pathPrefix("chapter" / IntNumber) { chapter =>
+      optionalCookie("counter") { counter =>
+        pathEndOrSingleSlash {
           complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
-            s"<html><body>chapter: $chapter, {${System.currentTimeMillis()}}</body></html>"))
+            s"<html><body>index: {${
+              System.currentTimeMillis()
+            }}</body></html>"))
         } ~
-        pathPrefix("article" / IntNumber) { article =>
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
-            s"<html><body>article: $article {${System.currentTimeMillis()}}</body></html>"))
-        }
+          (pathPrefix("chapter" / IntNumber) & setC(counter)) {
+            chapter =>
+              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
+                s"<html><body>chapter: $chapter, {${
+                  System.currentTimeMillis()
+                }}</body></html>"))
+          } ~
+          pathPrefix("article" / IntNumber) {
+            article =>
+              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
+                s"<html><body>article: $article {${
+                  System.currentTimeMillis()
+                }}</body></html>"))
+          } ~
+          (path("new") & deleteCookie("counter", path = "/")) {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
+              s"<html><body>new: {${
+                System.currentTimeMillis()
+              }}</body></html>"))
+          }
+      }
     } ~
       post {
         pathEndOrSingleSlash {
@@ -45,6 +53,11 @@ object Front extends App {
             """{"test":"test"}"""))
         }
       }
+
+  def setC(prev: Option[HttpCookiePair]) = prev match {
+    case Some(prevVal) => setCookie(HttpCookie("counter", value = s"${prevVal.value.toInt + 1}", path = Some("/")))
+    case None             => setCookie(HttpCookie("counter", value = "0"))
+  }
 
   val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", port = settings.port)
 
